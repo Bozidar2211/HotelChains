c2c272212282a7c628f8ebf6382e93e9a1cd3ef4
Bozidar2211 <bozidar22112003@gmail.com>
2024-11-06 13:26:33 +0100

    added JWT authorization

---
+++ src/HotelChainAPI/Authorization/JwtTokenHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace HotelChainAPI.Authorization
+{
+    public class JwtTokenHelper
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenHelper(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public string GenerateToken(string username)
+        {
+            var secretKey = _configuration["JwtSettings:SecretKey"];
+            if (string.IsNullOrEmpty(secretKey))
+            {
+                throw new InvalidOperationException("SecretKey is not configured.");
+            }
+
+            var expiryInMinutes = _configuration["JwtSettings:ExpiryInMinutes"];
+            if (string.IsNullOrEmpty(expiryInMinutes))
+            {
+                throw new InvalidOperationException("ExpiryInMinutes is not configured.");
+            }
+
+            var secretKeyBytes = Encoding.ASCII.GetBytes(secretKey);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] { new Claim(ClaimTypes.Name, username) }),
+                Expires = DateTime.UtcNow.AddMinutes(int.Parse(expiryInMinutes)),
+                Issuer = _configuration["JwtSettings:Issuer"],
+                Audience = _configuration["JwtSettings:Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(secretKeyBytes), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+
+
+}
---
+++ src/HotelChainAPI/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using HotelChainAPI.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelChainAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly JwtTokenHelper _jwtTokenHelper;
+
+        public AuthController(JwtTokenHelper jwtTokenHelper)
+        {
+            _jwtTokenHelper = jwtTokenHelper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel loginModel)
+        {
+            
+            if (loginModel.Username == "Bozidar" && loginModel.Password == "Bozidar123")
+            {
+                var token = _jwtTokenHelper.GenerateToken(loginModel.Username);
+
+                return Ok(new { Token = token });
+            }
+            return Unauthorized();
+        }
+    }
+
+    public class LoginModel
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
